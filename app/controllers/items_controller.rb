18568d2756425b96f957b956afa13d9caac3299e
goldensorcererofumineko <143801333+goldensorcererofumineko@users.noreply.github.com>
2023-10-08 07:14:23 +0900

    セキュリティー面からの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,8 +39,12 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    item.destroy
+    if item.user == current_user  
+    tem.destroy
     redirect_to root_path
+    else
+    redirect_to root_path
+    end
   end
 
   private
