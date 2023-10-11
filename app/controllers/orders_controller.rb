a157e2d7b5747d27b4b7417395eab274bda63900
goldensorcererofumineko <143801333+goldensorcererofumineko@users.noreply.github.com>
2023-10-11 17:51:57 +0900

    大まかな完成

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,40 @@
+class OrdersController < ApplicationController
+  def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+    @order_form = OrderForm.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_form = OrderForm.new(order_params)
+    if @order_form.valid?
+      pay_item
+      @order_form.save
+      return redirect_to root_path
+    else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+      render 'index', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_form).permit(:postcode, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.item_price, 
+      card: order_params[:token],   
+      currency: 'jpy'                 
+    )
+  end
+
+  def non_purchased_item
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
+  end
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   has_one_attached :image
   belongs_to :user
+  has_one    :order
 
   validates :item_info, presence: true
   validates :item_price, presence: true
