84e3befd97e159bfb015b310dbc6f79eb204decd
goldensorcererofumineko <143801333+goldensorcererofumineko@users.noreply.github.com>
2023-10-12 17:56:29 +0900

    環境修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,9 +1,19 @@
 window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
-   priceInput.addEventListener("input", () => {
-     const addTaxDom = document.getElementById("add-tax-price");
-     addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1 );
-     const addPofitDom = document.getElementById("profit");
-     addPofitDom.innerHTML = Math.floor(priceInput.value - Math.floor(priceInput.value * 0.1 ))
- })
+
+  if (priceInput !== null) { 
+    priceInput.addEventListener("input", () => {
+      const addTaxDom = document.getElementById("add-tax-price");
+      const addPofitDom = document.getElementById("profit");
+
+      if (addTaxDom !== null && addPofitDom !== null) { 
+        addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1);
+        addPofitDom.innerHTML = Math.floor(priceInput.value - Math.floor(priceInput.value * 0.1));
+      } else {
+        console.error("Element not found: add-tax-price or profit");
+      }
+    });
+  } else {
+    console.error("Element not found: item-price");
+  }
 });
